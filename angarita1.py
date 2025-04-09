cb5536dbb0a04ca66797b82f294cc0cdf33c4306
Byron1974 <byrondanielpenalozapena@gmail.com>
2025-04-09 17:05:46 -0500

    Determine el valor de verdad de las siguientes expresiones

---
+++ angarita1.py
@@ -0,0 +1,50 @@
+# Definir los valores
+result = (True and False) or (False or True)
+
+# Mostrar el resultado
+print(result)  # Imprime: True
+
+# Definir los valores
+result = (True and False) or (True and (False or True))
+
+# Mostrar el resultado
+print(result)  # Imprime: True
+
+# Evaluamos la expresión
+resultado = (4 > 6) and (10 == 10) and (3 % 3 <= 0)
+
+# Imprimimos el resultado
+print(f"El resultado es: {resultado}")
+
+# Evaluamos la expresión
+resultado = (7 != 8) and (5 * (8 - 9) > -3) and (4 == 5) or (6 != 5)
+
+# Imprimimos el resultado
+print(f"El resultado es: {resultado}")
+
+# Evaluamos la expresión
+resultado = (5 % 5 > 1 or not (5 == 4)) or (4 != 1) or (4 < -4)
+
+# Imprimimos el resultado
+print(f"El resultado es: {resultado}")
+
+# Evaluamos la expresión
+resultado = not (6 * -1 == 6) and (2 < 3 * -1) or (3 == 3) or (9 < 50 / 10) or not (18 == 2 * 9)
+
+# Imprimimos el resultado
+print(f"El resultado es: {resultado}")
+
+# Definimos los valores lógicos
+Falso = False
+Verdadero = True
+
+# Evaluamos la expresión
+resultado = (Falso != (2 > 1)) and (Verdadero == (4 < 2))
+
+# Imprimimos el resultado
+print(f"El resultado es: {resultado}")
+
+
+
+
+
